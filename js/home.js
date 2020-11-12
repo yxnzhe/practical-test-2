84d5419a92d0ad540a472a1aa20e4718325b0074
yxnzhe <71542921+yxnzhe@users.noreply.github.com>
2020-11-12 22:59:47 +0800

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,11 @@
+alert("Welcome Mr Chew, please enjoy!");
+
+function bigImg(x) {
+   x.style.height = "70%";
+   x.style.width = "70%";
+}
+
+function normalImg(x) {
+   x.style.height = "auto";
+   x.style.width = "auto";
+}
